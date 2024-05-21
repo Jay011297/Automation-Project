6369d2597125a290b302ab140b9eedb03e0ea6b6
Jay011297 <165775966+Jay011297@users.noreply.github.com>
2024-05-21 12:12:36 +0530

    Add files via upload

---
+++ src/test/java/Dropdown/HiddenDropdown.java
@@ -0,0 +1,15 @@
+package Dropdown;
+
+public class HiddenDropdown {
+
+	public static void main(String[] args) {
+		
+		//Bootstrap dropdwon is the multi select dropdwon where user can select multiple option at same time
+		// Bootstrap dropdown have div/button tag in DOM 
+		//There are no in-build methods in bootstrap dropdwons
+		
+		
+
+	}
+
+}
